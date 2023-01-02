b3eb09507030ab2e820f03a908c228e002839c81
Osamu Ito <mr.samito@gmail.com>
2023-01-02 16:45:27 -0500

    add skeleton about info

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,59 @@
+import cole from "./assets/cole.png";
+import katie from "./assets/katie.png";
+
 function About() {
-  return <div>This is the About Section</div>;
+  return (
+    <div className="about">
+      <div>
+        <p>Experience you can trust</p>
+        <p className="infop">
+          Our team combines experienced recruiters with startup operators who
+          have founded, scaled, and excited companies.
+        </p>
+
+        <div className="founder_about">
+          <div className="founder_images">
+            <img className="katie_img" src={katie} alt="katie" />
+          </div>
+          <div>
+          <p>Katie Zucker</p>
+          <p className="infop">
+            Katie is an experienced HR Leader and Los Angeles based
+            Entrepreneur. She brings over 15 years of experience in hiring,
+            building teams, and advising on organizational structure for
+            companies such as Bridgewater, Adobe, Salesforce, and First
+            Quadrant. Katie has helped many companies grow to the next level by
+            introducing the right talent at the right time. She also previously
+            founded and successfully sold Hey Hero, a Creator monetization
+            platform, acquired by Sanctuary Ventures. She is a proud alumna of
+            Columbia Business School and Tufts University.
+          </p>
+          </div>
+          </div>
+        <div className="founder_about">
+          <div className="founder_images">
+            <img className="cole_img" src={cole} alt="cole" />
+          </div>
+          <div>
+          <p>Cole Zucker</p>
+          <p className="infop">
+            Cole is a serial Los Angeles-based entrepreneur with multiple
+            successful exits. He is best known for founding GREEN CREATIVE in
+            2011, an LED lighting company with HQ’s in Shanghai and San
+            Francisco. Fully bootstrapped, Green Creative grew to be one of the
+            largest commercial LED lighting companies, before successfully
+            exiting to Harbour Group Capital in 2018. Cole has extensive
+            experience in scaling up high growth teams. He understands first
+            hand the needs of a growing organization, and has hired hundreds of
+            employees across the businesses he has built. Cole is an active
+            member of YPO and in the Santa Monica Chapter.
+          </p>
+        </div>
+        </div>
+        <p>Hire Talent Employer Page</p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,14 +1,42 @@
 function Info() {
   return (
     <div className="info">
-      <div className="section">
+      <div className="infosection">
         <p>
-          How we are&nbsp;<em>different</em>
+          How we are &nbsp;<em>different</em>
         </p>
         <button className="button2">Hire Talent</button>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <p className="infop">
+          As Founders ourselves, we know fast time to hire is critical to
+          scaling your business. Hiring a top performer can be a 100x
+          multiplier. Hiring the wrong candidate can waste critical time and
+          money.
+          <br />
+          <br />
+          Our process has helped build some of the most successful teams.
+          <br />
+          <br />
+          Before we initiate a search, we take a 360 approach to understanding
+          your organization, culture, and position.
+        </p>
+      </div>
+      <div>
+        <p className="infop">
+          We put our experience and networks to work for you engaging and
+          exciting candidates who are not actively seeking new employment.
+          <br />
+          <br />
+          We believe focus and dedication are central to providing the most
+          effective Client Service.
+          <br />
+          <br />
+          At New Talent Group we actively limit the number of Clients we support
+          at any time. When you work with us, you know you are receiving direct
+          partnership from one of our Managing Partners.
+        </p>
+      </div>
     </div>
   );
 }
