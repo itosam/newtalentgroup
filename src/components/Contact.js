ab6fff14a66ff23314da5de1eeb43a0a71ad26e3
Osamu Ito <mr.samito@gmail.com>
2023-01-03 17:00:07 -0500

    added contact section skeleton

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,24 @@
 function Contact() {
-  return <div>This is the Contact section</div>;
+  return (
+    <div className="contact">
+      <div className="hiresection">
+        <p>
+          Hiring the best people is your most important task. We’re here to
+          help.
+        </p>
+        <button className="button2">Get in touch</button>
+      </div>
+      <div className="contactform">
+        <p>Build your team with New Talent Group</p>
+        <p className="infop">
+          Let’s connect! We look forward to learning about the growth of your
+          business.
+        </p>
+        <form></form>
+        <button className="button1">Submit</button>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
