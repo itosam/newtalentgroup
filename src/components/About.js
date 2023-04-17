18f2227fc318fd61513bfab5cee157b6f8f18f19
Osamu Ito <mr.samito@gmail.com>
2023-04-17 17:48:56 -0400

    update contact

--- src/components/About.js
+++ src/components/About.js
@@ -32,12 +32,16 @@ function About() {
               Entrepreneur. She brings over 15 years of experience in hiring,
               building teams, and advising on organizational structure for
               companies such as Bridgewater, Adobe, Salesforce, and First
-              Quadrant. <br />
+              Quadrant.
+              <br />
+              <br />
               Katie has helped many companies grow to the next level by
               introducing the right talent at the right time.
               <br />
+              <br />
               She also previously founded and successfully sold Hey Hero, a
               Creator monetization platform, acquired by Sanctuary Ventures.
+              <br />
               <br />
               She is a proud alumna of Columbia Business School and Tufts
               University.
@@ -66,10 +70,12 @@ function About() {
               the largest commercial LED lighting companies, before successfully
               exiting to Harbour Group Capital in 2018.
               <br />
+              <br />
               Cole has extensive experience in scaling up high growth teams. He
               understands first hand the needs of a growing organization, and
               has hired hundreds of employees across the businesses he has
               built.
+              <br />
               <br />
               Cole is an active member of YPO and in the Santa Monica Chapter.
             </p>
